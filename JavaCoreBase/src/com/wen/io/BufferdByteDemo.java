d515237261f86a7a80db617060cc5243550ecf2b
wenqinsong <fasterlogin@qq.com>
2017-09-06 21:12:59 +0800

    add io3

---
+++ JavaCoreBase/src/com/wen/io/BufferdByteDemo.java
@@ -0,0 +1,15 @@
+package com.wen.io;
+
+import java.io.File;
+
+public class BufferdByteDemo {
+
+	public static void main(String[] args) {
+		// 创建文件对象
+		File file =new File("D:\\github\\java6\\HelloWorld.java");
+		File file2 =new File("D:\\github\\java4\\HelloWorld,java");
+		
+
+	}
+
+}
